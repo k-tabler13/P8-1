29cf73bb776b45ab28ed282adf4c1670d21bc429
k-tabler13 <k-tabler@onu.edu>
2024-10-31 09:11:33 -0400

    completed character functions

---
+++ P8-1/P8-1.cpp
@@ -0,0 +1,14 @@
+// counts the amount of vowels in given letters 
+
+#include <iostream> 
+#include <string>
+
+using namespace std;
+
+//functions and global var here 
+
+int countVowel(string str);
+
+//need to fix Q
+bool Q = false;
+
