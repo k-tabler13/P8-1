b8d26892e8d3598d8c7cf69dcad9ba01eeb6d1aa
k-tabler13 <k-tabler@onu.edu>
2024-10-31 09:12:25 -0400

    completed countVowel function

--- P8-1/P8-1.cpp
+++ P8-1/P8-1.cpp
@@ -12,3 +12,38 @@ int countVowel(string str);
 //need to fix Q
 bool Q = false;
 
+
+//int main here 
+int main(void) {
+
+	string letter;
+
+	string input;
+	while (true) {
+		cout << "Enter a string or Q to quit: ";
+		getline(cin, input);
+		if (input == "Q") break;
+		cout << "Vowel count: " << countVowel(input) << endl; // do something with input
+
+	}
+	return 0;
+
+
+
+
+}
+
+//function definitions here  
+
+int countVowel(string str) {
+
+	int count = 0;
+	for (char ch : str) {
+		if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+			ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+			count++;
+		}
+	}
+	return count;
+
+}
